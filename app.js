b8f76e77b938f458354d16a48949c658ab21025c
mackoyokcam <mark.hangarter@gmail.com>
2017-07-17 13:24:43 -0700

    Initialized object literals for each location. Added placeholder comment for calculations. Initialized sales.html

---
+++ app.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var firstAndPike = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3,
+  generateRandom: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+  },
+  calculateCookie: function() {
+    // Calculate cookie stuff here
+  },
+  results: [],
+};
+
+var seaTacAirport = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3,
+  generateRandom: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+  },
+  calculateCookie: function() {
+    // Calculate cookie stuff here
+  },
+  results: [],
+};
+
+var seattleCenter = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3,
+  generateRandom: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+  },
+  calculateCookie: function() {
+    // Calculate cookie stuff here
+  },
+  results: [],
+};
+
+var capitolHill = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3,
+  generateRandom: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+  },
+  calculateCookie: function() {
+    // Calculate cookie stuff here
+  },
+  results: [],
+};
+
+var alki = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3,
+  generateRandom: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+  },
+  calculateCookie: function() {
+    // Calculate cookie stuff here
+  },
+  results: [],
+};
+
+firstAndPike.calculateCookie();
+seaTacAirport.calculateCookie();
+seattleCenter.calculateCookie();
+capitolHill.calculateCookie();
+alki.calculateCookie();
