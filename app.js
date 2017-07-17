d9a4f9374008423d1eada41b8fa4f4c490254ce3
mackoyokcam <mark.hangarter@gmail.com>
2017-07-17 14:24:34 -0700

    Setup list for sales.html, first and pike calculations working, displays on sales.html

--- app.js
+++ app.js
@@ -1,72 +1,108 @@
 'use strict';
 
+// For future use
+var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+/* Store-location objects */
+
 var firstAndPike = {
   minCust: 23,
   maxCust: 65,
   avgCookie: 6.3,
+  openHour: 6,
+  closeHour: 20,
   generateRandom: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
   },
-  calculateCookie: function() {
-    // Calculate cookie stuff here
+  render: function() {
+    var firstAndPikeUl = document.getElementById('firstAndPike');
+    // Iterate over each day
+    var hoursOpen = Math.abs(this.closeHour - this.openHour);
+    for(var i = 0; i < hoursOpen; i++) {
+      // There are three parts to this process:
+      // 1. Create an element
+      var liEl = document.createElement('li');
+      // 2. Give it content
+      var numCookies = Math.round(this.avgCookie * this.generateRandom());
+      var time;
+      if (i + this.openHour <= 11) {
+        time = (this.openHour + i) + 'am';
+      } else if (i + this.openHour === 12) {
+        time = (this.openHour + i) + 'pm';
+      } else {
+        time = (this.openHour + i - 12) + 'pm';
+      }
+      liEl.textContent = time + ' ' + numCookies;
+      // 3. Append it to a certain place in the DOM
+      // parentElement.appendChild(childElement)
+      firstAndPikeUl.appendChild(liEl);
+    }
   },
   results: [],
 };
 
 var seaTacAirport = {
-  minCust: 23,
-  maxCust: 65,
-  avgCookie: 6.3,
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2,
+  openHour: 6,
+  closeHour: 20,
   generateRandom: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
   },
-  calculateCookie: function() {
+  render: function() {
     // Calculate cookie stuff here
   },
   results: [],
 };
 
 var seattleCenter = {
-  minCust: 23,
-  maxCust: 65,
-  avgCookie: 6.3,
+  minCust: 11,
+  maxCust: 38,
+  avgCookie: 3.7,
+  openHour: 6,
+  closeHour: 20,
   generateRandom: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
   },
-  calculateCookie: function() {
+  render: function() {
     // Calculate cookie stuff here
   },
   results: [],
 };
 
 var capitolHill = {
-  minCust: 23,
-  maxCust: 65,
-  avgCookie: 6.3,
+  minCust: 20,
+  maxCust: 38,
+  avgCookie: 2.3,
+  openHour: 6,
+  closeHour: 20,
   generateRandom: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
   },
-  calculateCookie: function() {
+  render: function() {
     // Calculate cookie stuff here
   },
   results: [],
 };
 
 var alki = {
-  minCust: 23,
-  maxCust: 65,
-  avgCookie: 6.3,
+  minCust: 2,
+  maxCust: 16,
+  avgCookie: 4.6,
+  openHour: 6,
+  closeHour: 20,
   generateRandom: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
   },
-  calculateCookie: function() {
+  render: function() {
     // Calculate cookie stuff here
   },
   results: [],
 };
 
-firstAndPike.calculateCookie();
-seaTacAirport.calculateCookie();
-seattleCenter.calculateCookie();
-capitolHill.calculateCookie();
-alki.calculateCookie();
+firstAndPike.render();
+// seaTacAirport.render();
+// seattleCenter.render();
+// capitolHill.render();
+// alki.render();
