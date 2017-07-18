5873f238d66e9e192ef38295c34394d3cee83fe9
mackoyokcam <mark.hangarter@gmail.com>
2017-07-18 13:24:33 -0700

    commented out old html, added CSS, added totals per hour feature

--- app.js
+++ app.js
@@ -106,31 +106,51 @@ for (var key in storeList) {
     currentStore.avgCookie, currentStore.openHour, currentStore.closeHour));
 }
 
-/* Render Header */
+/* Render Header row */
+function renderHeader() {
+  // Location
+  var tableElement = document.getElementById(tableName);
+  var headerElement = document.createElement('th');
+  headerElement.textContent = 'Location';
+  tableElement.appendChild(headerElement);
 
-// Location
-var tableElement = document.getElementById(tableName);
-var headerElement = document.createElement('th');
-headerElement.textContent = 'Location';
-tableElement.appendChild(headerElement);
+  // Time
+  for(var i = 0; i < hours.length; i++) {
+    headerElement = document.createElement('th');
+    headerElement.textContent = hours[i];
+    tableElement.appendChild(headerElement);
+  }
 
-// Time
-for (var i = 0; i < hours.length; i++) {
+  // Total
   headerElement = document.createElement('th');
-  headerElement.textContent = hours[i];
+  headerElement.textContent = 'Total';
   tableElement.appendChild(headerElement);
 }
+renderHeader();
 
-// Total
-headerElement = document.createElement('th');
-headerElement.textContent = 'Total';
-tableElement.appendChild(headerElement);
-
-// Render the stores
-for (var i = 0; i < patStores.length; i++) {
+/* Render the store rows */
+for(var i = 0; i < patStores.length; i++) {
   patStores[i].addToTable();
 }
 
+/* Render Total row */
+
+// The word 'Total'
+var tableElement = document.getElementById(tableName);
+var rowElement = document.createElement('td');
+rowElement.textContent = 'Total';
+tableElement.appendChild(rowElement);
+
+// The data for total
+for(i = 0; i < hours.length; i++) {
+  rowElement = document.createElement('td');
+  var totalCookiesPerHour = 0;
+  for (var j = 0; j < patStores.length; j++) {
+    totalCookiesPerHour += patStores[j].totalCookies[i];
+  }
+  rowElement.textContent = totalCookiesPerHour;
+  tableElement.appendChild(rowElement);
+}
 
 // Logic for displaying the time of day (Version 1)
 // var time;
